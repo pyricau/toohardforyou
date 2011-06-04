f58f1cb62b24460b394357c51b4efba5b2f196ae
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-06-04 11:58:18 +0200

    changing entrypoint name

--- toohardforyou-core/src/main/java/info/piwai/toohardforyou/client/TooHardForYouEntryPoint.java
+++ toohardforyou-core/src/main/java/info/piwai/toohardforyou/client/TooHardForYouEntryPoint.java
@@ -2,7 +2,7 @@ package info.piwai.toohardforyou.client;
 
 import com.google.gwt.core.client.EntryPoint;
 
-public class TooHardForYou implements EntryPoint {
+public class TooHardForYouEntryPoint implements EntryPoint {
 
     public void onModuleLoad() {
 
