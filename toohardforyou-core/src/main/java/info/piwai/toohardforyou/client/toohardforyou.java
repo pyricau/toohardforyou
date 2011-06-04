bd7ccc4a07ec95fa8b731ee07da3ba487e8aa1fc
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-06-04 10:28:35 +0200

    importing project generated with maven gwt plugin and tweaked for gwt 2.3.0

---
+++ toohardforyou-core/src/main/java/info/piwai/toohardforyou/client/toohardforyou.java
@@ -0,0 +1,10 @@
+package info.piwai.toohardforyou.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class TooHardForYou implements EntryPoint {
+
+    public void onModuleLoad() {
+
+    }
+}
