d688e0c2eacf8c82b2e145eafffd6838799830c9
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-06-05 08:23:07 +0200

    cleaning timer

--- toohardforyou-core/src/main/java/info/piwai/toohardforyou/core/Timer.java
+++ toohardforyou-core/src/main/java/info/piwai/toohardforyou/core/Timer.java
@@ -1,21 +1,5 @@
 package info.piwai.toohardforyou.core;
 
-/*
- * Copyright 2007 Google Inc.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 import static forplay.core.ForPlay.currentTime;
 
 import java.util.ArrayList;
@@ -23,7 +7,7 @@ import java.util.ArrayList;
 import forplay.core.Game;
 
 /**
- * A simplified, browser-safe timer class. This class serves the same purpose as
+ * A simplified timer class. This class serves the same purpose as
  * java.util.Timer, but is simplified because of the single-threaded
  * environment.
  * 
